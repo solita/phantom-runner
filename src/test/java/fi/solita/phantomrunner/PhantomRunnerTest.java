e785122f2e2c5bbe46b14b40d6a36c46f3311f4f
niklasco <none@none>
2012-09-21 12:14:58 +0300

    Adds a bit more assertions to the PhantomRunnerTest
    --HG--
    branch : jetty-integration

--- src/test/java/fi/solita/phantomrunner/PhantomRunnerTest.java
+++ src/test/java/fi/solita/phantomrunner/PhantomRunnerTest.java
@@ -1,22 +1,47 @@
 package fi.solita.phantomrunner;
 
+import static org.junit.Assert.*;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.Test;
 import org.junit.runner.JUnitCore;
 import org.junit.runner.Result;
-
+import org.junit.runner.notification.Failure;
+import org.junit.runner.notification.RunListener;
 
 public class PhantomRunnerTest {
     
     @Test
     public void testRunnerWithJasmineTests() {
-        Result result = new JUnitCore().run(PhantomRunnerTestHolder.class);
+        JUnitCore core = new JUnitCore();
+        
+        final List<String> failedTestNames = new ArrayList<>();
+        
+        core.addListener(new RunListener() {
+            @Override
+            public void testFailure(Failure failure) throws Exception {
+                String header = failure.getTestHeader();
+                failedTestNames.add(header.substring(0, header.indexOf('(')));
+            } 
+        });
+        
+        Result result = core.run(PhantomRunnerTestHolder.class);
         
         // 6 == PhantomRunnerTestHolder + 5 Jasmine specs
         assertEquals(6, result.getRunCount());
         assertEquals(2, result.getFailureCount());
+        
+        // failure listener call count should equal to result.getFailureCount()
+        assertEquals(result.getFailureCount(), failedTestNames.size());
+        
+        // specific tests should have failed
+        assertTrue(failedTestNames.contains("and also it may fail too"));
+        assertTrue(failedTestNames.contains("and some specifications which may fail"));
+        
         assertFalse(result.wasSuccessful());
     }
 }
